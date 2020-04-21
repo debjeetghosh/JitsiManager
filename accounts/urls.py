48d213c68fd0c0ade9dba22804dc4f37a8791263
nati <shourav.nath@priyo.com>
2020-04-22 00:38:35 +0600

    register is removed

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from accounts.views import *
 
 app_name = 'accounts'
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
+    # path('register/', RegisterView.as_view(), name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('login/submit/', login_submit, name='login_submit'),
