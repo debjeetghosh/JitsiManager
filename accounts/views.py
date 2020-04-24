6ae820f8eb8a0596077c87cd2d470088da54c4b6
nati <shourav.nath@priyo.com>
2020-04-24 23:55:15 +0600

    otp only for admin

--- accounts/views.py
+++ accounts/views.py
@@ -259,12 +259,16 @@ def login_submit(request):
                 login(request, user)
                 if 'next' in request.GET:
                     return redirect(request.GET.get('next', '/'))
-                # return redirect(reverse('accounts:dashboard'))
+                if user.profile.user_type == UserProfile.CREATOR:
+                    return redirect(reverse('accounts:dashboard'))
                 return redirect(reverse('accounts:verify-otp'))
             elif email_user and getattr(email_user, 'is_active', False):
                 user = authenticate(username=email_user.username, password=password)
                 if user:
                     login(request, user)
+
+                    if user.profile.user_type == UserProfile.CREATOR:
+                        return redirect(reverse('accounts:dashboard'))
                     if 'next' in request.GET:
                         return redirect(request.GET.get('next', '/'))
                     # return redirect(reverse('accounts:dashboard'))
--- jitsi_helper/middleware.py
+++ jitsi_helper/middleware.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect
 from django.urls import reverse
 
+from accounts.models import UserProfile
+
 
 class OTPAuthMiddleware(object):
     def __init__(self, get_response):
@@ -10,6 +12,6 @@ class OTPAuthMiddleware(object):
         response = self.get_response(request)
         if request.session.get('email_verified', False):
             return response
-        if request.user.is_authenticated and request.path != '/verify-otp/':
+        if request.user.is_authenticated and request.user.profile.user_type==UserProfile.ADMIN and request.path != '/verify-otp/':
             return redirect(reverse('accounts:verify-otp'))
         return response
